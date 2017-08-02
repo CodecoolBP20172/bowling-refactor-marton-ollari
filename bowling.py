fb3a84662e8449e0d5b9405e71f5266769fea31d
Ollári Márton <ollarimarton@gmail.com>
2017-08-02 16:28:31 +0200

    refactor

--- bowling.py
+++ bowling.py
@@ -3,43 +3,29 @@ def score(game):
     frame = 1
     in_first_half = True
     for i in range(len(game)):
-        if game[i] == '/':
-            result += 10 - last
-        else:
-            result += get_value(game[i])
-        # if not in_first_half:
-            # frame += 1
-        if frame < 10  and get_value(game[i]) == 10:
-            if game[i] == '/':
-                result += get_value(game[i+1])
-            elif game[i] == 'X' or game[i] == 'x':
-                result += get_value(game[i+1])
-                if game[i+2] == '/':
-                    result += 10 - get_value(game[i+1])
-                else:
-                    result += get_value(game[i+2])
-        last = get_value(game[i])
-        if not in_first_half:
+        result += check_spare(game, i)
+        if frame < 10 and game[i] in ['x', 'X', '/']:
+            if game[i].lower() == 'x':
+                result += check_spare(game, i+2)
+            result += get_value(game[i+1])
+        if not in_first_half or game[i].lower() == 'x':
             frame += 1
-        if in_first_half == True:
+            in_first_half = True
+        else:
             in_first_half = False
-        else:
-            in_first_half = True
-        if game[i] == 'X' or game[i] == 'x':
-            in_first_half = True
-            frame += 1
     return result
 
-def get_value(char):
-    if char == '1' or char == '2' or char == '3' or \
-       char == '4' or char == '5' or char == '6' or \
-       char == '7' or char == '8' or char == '9':
-        return int(char)
-    elif char == 'X' or char == 'x':
-        return 10
-    elif char == '/':
-        return 10
-    elif char == '-':
-        return 0
+
+def check_spare(game, spare):
+    if game[spare] == '/':
+        return (10 - get_value(game[spare-1]))
     else:
-        raise ValueError()
+        return get_value(game[spare])
+
+
+def get_value(char):
+    if char.isdigit():
+        return int(char)
+    elif char in ['x', 'X', '/']:
+        return 10
+    return 0
